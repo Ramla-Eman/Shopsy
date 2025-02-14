95183ee14e397e5c12aaa5a9441ae5a1eac06896
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-02-14 16:58:02 +0500

    complete footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Navbar from "./Components/Navbar";
 import Produts from "./Components/Produts";
 // Import AOS for animation
 import AOS from "aos";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 import TopProducts from "./Components/TopProducts";
 import Banner from "./Components/Banner";
 import Subscribe from "./Components/Subscribe";
 import Testimonials from "./Components/Testimonials";
+import Footer from "./Components/Footer";
 
 function App() {
   React.useEffect(() => {
@@ -22,14 +23,17 @@ function App() {
   }, []);
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Produts />
-      <TopProducts/>
-      <Banner/>
-      <Subscribe/>
-      <Produts />
-      <Testimonials/>
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar />
+        <Hero />
+        <Produts />
+        <TopProducts />
+        <Banner />
+        <Subscribe />
+        <Produts />
+        <Testimonials />
+        <Footer />
+      </div>
     </>
   );
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
+import Banner from "../assets/footer-pattern.jpg";
+import footerLogo from "../assets/logo.png";
+import { FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow, FaMobileAlt } from "react-icons/fa";
+
+const BannerImg = {
+  backgroundImage: `url(${Banner})`,
+  backgroundPosition: "bottom",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+  width: "100%",
+};
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div style={BannerImg} className="text-white">
+      <div data-aos="zoom-in" className="p-[1rem] lg:px-[14rem] py-2">
+        <div className="grid md:grid-cols-3 pb-44 pt-5">
+          {/* Company Details */}
+          <div className="py-8 px-4">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={footerLogo} alt="" className="max-w-[50px]" />
+              Shopsy
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in
+              beatae ea recusandae blanditiis veritatis.
+            </p>
+          </div>
+          {/* Footer links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="text-xl font-semibold sm:text-left text-justify md-3">
+                  Important Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:text-primary text-gray-200">
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="text-xl font-semibold sm:text-left text-justify md-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:text-primary text-gray-200">
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            {/* Social Links */}
+            <div>
+              <div className="flex items-center gap-3 mt-6">
+                <a href="#">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow/>
+                  <p>Karachi, Pakistan</p>
+                </div>  
+                <div className="flex items-center gap-3 mt-3">
+                  <FaMobileAlt/>
+                  <p>+92 312 0306090</p>
+                </div>  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
